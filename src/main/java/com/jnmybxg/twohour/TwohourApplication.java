2c91f70b211d62e801e453da3aea1866adaf91a8
geekfly <geekfly123@163.com>
2019-05-26 21:26:49 +0800

    添加注释,来测试一下git修改

--- src/main/java/com/jnmybxg/twohour/TwohourApplication.java
+++ src/main/java/com/jnmybxg/twohour/TwohourApplication.java
@@ -11,6 +11,7 @@ public class TwohourApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TwohourApplication.class, args);
+        System.out.print("d");
     }
 
 }
